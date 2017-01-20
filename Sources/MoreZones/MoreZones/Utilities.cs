8d98f074916521f9c8c701afa2a06a5359f92958
Derek Webb <dwebb.af@gmail.com>
2017-01-20 00:11:39 -0500

    Added function to create Command_Action

--- Sources/MoreZones/MoreZones/Utilities.cs
+++ Sources/MoreZones/MoreZones/Utilities.cs
@@ -28,30 +28,63 @@ namespace MoreZones
             }
         }
 
+		public static Command_Action CreateCommandAction(string label, string description, string icon, Action action, KeyBindingDef keyBindingDef = null)
+		{
+			Command_Action command = new Command_Action ();
+			command.defaultLabel = label;
+			command.defaultDesc = description;
+			command.icon = ContentFinder<Texture2D>.Get (icon, true);
+			command.action = action;
+
+			if (keyBindingDef != null)
+			{
+				command.hotKey = keyBindingDef;
+			}
+
+			return command;
+		}
+
+		//public static Command_Toggle
+
 		private static Zone_Stockpile currentZoneStockpile = null;
+		//private static ZoneManager zoneManager = null;
 
 		[DetourMethod(typeof(Zone_Stockpile), "GetGizmos")]
-		private static IEnumerable<Gizmo> GetGizmos(this Zone_Stockpile self) {
+		private static IEnumerable<Gizmo> GetGizmos(this Zone_Stockpile self)
+		{
 			currentZoneStockpile = self;
 
 			List<Gizmo> gizmos = new List<Gizmo> ();
 
-			Command_Action command = new Command_Action ();
-			command.icon = ContentFinder<Texture2D>.Get ("UnSkilled", true);
-			command.action = new Action (DoThing);
-			command.defaultLabel = "Test";
-			command.defaultDesc = "Test description";
+			// Add rename command
+			Command_Action renameCommandAction = Utilities.CreateCommandAction (
+				"Rename",
+				"Rename this zone.",
+				"UI/PasteSettings",
+				new Action(RenameZone),
+				KeyBindingDef.Named ("B")
+			);
 
-			gizmos.Add (command);
+//			Command_Action
+//			command.icon = ContentFinder<Texture2D>.Get ("UnSkilled", true);
+//			command.action = new Action (DoThing);
+//			command.defaultLabel = "Test";
+//			command.defaultDesc = "Test description";
 
-			//return self.GetGizmos ().Concat (new Gizmo[] { command });
+			gizmos.Add (renameCommandAction);
 
 			return gizmos;
 		}
 
-		private static void DoThing()
+		private static void RenameZone()
 		{
-			Log.Message ("Zone: " + currentZoneStockpile.label);
+			//Log.Message ("Zone: " + currentZoneStockpile.label);
+
+			//Find.WindowStack.Add (new Dialog_RenameZone (currentZoneStockpile));
+
+			foreach (Texture2D texture in ContentFinder<Texture2D>.GetAllInFolder ("UI")) {
+				Log.Message (texture.name);
+			}
 		}
     }
 }
